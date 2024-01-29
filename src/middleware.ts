2dc22b47597451b42963597e7a9093a7f3afac1f
Roy Anger <roy@royanger.com>
2024-01-28 22:45:28 -0500

    Swamped example to @clert/nextjs v4

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,37 +1,37 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-const isProtectedRoute = createRouteMatcher(["/dashboard", "/onboarding"])
-
+import { authMiddleware } from "@clerk/nextjs";
+import { redirectToSignIn } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+export default authMiddleware({
+  publicRoutes: ["/"],
+  afterAuth: async (auth, req: NextRequest) => {
+    const { userId, sessionClaims } = auth
 
+    // For user visiting /onboarding, don't try and redirect
+    if (userId && req.nextUrl.pathname === "/onboarding") {
+      return NextResponse.next();
+    }
 
-export default clerkMiddleware((auth, req) => {
-  const userAuth = auth();
-  const isOnboarding = req.nextUrl.pathname === "/onboarding";
+    // User isn't signed in and the route is private -- redirect to sign-in
+    if (!userId && !auth.isPublicRoute) return redirectToSignIn({ returnBackUrl: req.url })
 
-  // If user is logged in
-  if (userAuth.userId) {
-
-    // If visiting /onboarding and onboarding is not complete, redirect
-    if (!isOnboarding && userAuth.sessionClaims?.metadata?.onboardingComplete !== true) {
+    // Catch users who doesn't have `onboardingComplete: true` in PublicMetata
+    // Redirect them to the /onboading out to complete onboarding
+    if (userId && !sessionClaims?.metadata?.onboardingComplete) {
       const onboardingUrl = new URL("/onboarding", req.url);
       return NextResponse.redirect(onboardingUrl)
     }
 
-    // If onboarding is complete or visiting another page, proceed
-    return NextResponse.next();
-  }
+    // User is logged in and the route is protected - let them view.
+    if (userId && !auth.isPublicRoute) return NextResponse.next()
 
-  // If user is not logged in and visiting a protected route, redirect to sign in
-  if (isProtectedRoute(req)) {
-    return userAuth.redirectToSignIn();
-  }
+    // If the route is public, anyone can view it.
+    if (auth.isPublicRoute) return NextResponse.next()
 
-  // User isn't logged in and the route is public - let them visit it
-  return NextResponse.next();
+  }
 });
 
 export const config = {
